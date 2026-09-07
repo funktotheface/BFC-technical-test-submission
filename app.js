b70fa708ee794fbbf152d9cbea0fc9784b3dbd02
Douglas <douglas.mottershead@outlook.com>
2026-09-07 10:12:29 +0100

    set application state and DOM element references

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
-const loadCourses = () => {
-    // Load the first 10 courses
-    const coursesData = loadMockCourses().slice(0, 10);
-    const coursesContainer = document.getElementById('courses');
+//set application state and register required DOM elements
 
-    // Clear existing courses
-    coursesContainer.innerHTML = '';
 
-    // Create and append course cards
-    for (const course of coursesData) {
-        const courseElement = document.createElement('article');
-        courseElement.innerHTML = `
-            <h2>${course.course_title}</h2>`;
-        coursesContainer.appendChild(courseElement);
-    }
+const PAGE_SIZE = 10;
+const allCourses = loadMockourses;
 
+//state is source of truth, search parameters are mapped to the state object.
+//this allows easy reliable access to the users desired search terms throughout the script
+const state = {
+    search: '',
+    level: 'all',
+    type: 'all',
+    subject: 'all',
+    sort: 'title-asc',
+    page: 1
 };
 
-// Load courses on page load
-loadCourses();
+//setting up references to DOM elements to be used throughout the script
+const searchInput = document.getElementById('search');
+const searchButton = document.getElementById('search-button');
 
+const levelFilter = document.getElementById('level-filter');
+const typeFilter = document.getElementById('type-filter');
+const subjectFilter = document.getElementById('subject-filter');
 
+const sortSelect = document.getElementById('sort');
 
+const coursesContainer = document.getElementById('courses');
+const resultsCounter = document.getElementById('results-counter');
+const paginationContainer = document.getElementById('pagination');
+const noResultsMessage = document.getElementById('no-results');
